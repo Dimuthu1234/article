2eb7b0375bbc63904c5b24aee94b23869ff23f9a
d.jayalath@stylesoftware.co <d.jayalath@stylesoftware.co>
2018-10-19 18:57:57 +0530

    dd

--- resources/views/article_show.blade.php
+++ resources/views/article_show.blade.php
@@ -46,6 +46,14 @@
             <div style="height: auto">
                 {!! $article->description !!}
             </div>
+            <div id="fb-root"></div>
+            <script>(function(d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) return;
+                    js = d.createElement(s); js.id = id;
+                    js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.1';
+                    fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));</script>
             <div class="fb-comments" data-href="{{ url('article/'.$article->slug) }}" data-numposts="10"></div>
         </div>
     </div><!-- col-sm-6 -->
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,20 +2,13 @@
 <html lang="en">
 @include('shared._head')
 <body>
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.1';
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
+
     @include('shared._header')
 {{--</div>--}}
 
     <section>
         <div class="container">
-            <div id="app">
+            {{--<div id="app">--}}
             <div class="row">
 
                 <div class="col-md-12">
@@ -28,7 +21,7 @@
                 {{--@include('shared._sidemenu')--}}
 
             </div><!-- row -->
-            </div>
+            {{--</div>--}}
         </div><!-- container -->
     </section>
 
